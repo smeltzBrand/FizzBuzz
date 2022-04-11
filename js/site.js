379179bd74e2b42ab3790eafb9780cb6063861b5
smeltzBrand <101213259+smeltzBrand@users.noreply.github.com>
2022-04-11 18:31:26 -0400

    Added Triple Array functions

--- js/site.js
+++ js/site.js
@@ -1,28 +1,79 @@
-
 //Step ONE - controller accept requests
-function getMessage(){
+function getValues() {
 
-    let msg = document.getElementById("txtMessage").value;
+    // First get the fizz anb buzz values from the user
+    let fizzValue = document.getElementById("fizzNum").value;
+    let buzzValue = document.getElementById("buzzNum").value;
 
-    displayMessage(msg);
+    // check if the user entered numbers
+    fizzValue = parseInt(fizzValue);
+    buzzValue = parseInt(buzzValue);
+
+
+    if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue)) {
+        //Call display values and pass fizz buzz values to the function
+        //calculate fizzbuzz
+        let fbValues = fizzBuzzA(fizzValue, buzzValue);
+        //
+        displayValues(fbValues);
+    } else {
+        alert("You must enter integers!")
+    }
 
 }
 
+//business logic layer - Model
+function fizzBuzzA(fizz, buzz) {
+
+    // create an array
+    let fbValues = [];
+
+    for (let i = 1; i < 100; i++) {
+
+        if (i % fizz == 0 && i % buzz == 0) {
+            fbValues.push('FizzBuzz');
+        } else if (i % fizz == 0) {
+            fbValues.push('Fizz');
+        } else if (i % buzz == 0) {
+            fbValues.push('Buzz');
+        } else {
+            fbValues.push(i);
+        }
+
+    }
+
+    return fbValues;
+
+}
 
 //Final Step - view
-function displayMessage(message) {
-    
-    //first get the ol element from the page
-    element = document.getElementById("results");
-    
-    //next create a new li element
-    let item = document.createElement("li");
-    //add classes to the li element
-    item.classList.add("list-group-item");
-    //set the message for the li element
-    item.innerHTML = message;
+function displayValues(fbValues) {
 
-    //add the new item to the list
-    element.appendChild(item);
+    // display a list of numbers from 1 to 100
+
+    let element = document.getElementById("results");
+
+    element.innerHTML = "";
+
+
+    // write a foor loop to display from 1 to 100
+    for (let i = 0; i < fbValues.length; i++) {
+
+        let item = document.createElement("div");
+        item.classList.add("col");
+        item.innerHTML = fbValues[i];
+
+        if (fbValues[i] == "FizzBuzz") {
+            item.classList.add("purpleItem")
+        } else if (fbValues[i] == "Fizz") {
+            item.classList.add("redItem")
+        } else if (fbValues[i] == "Buzz") {
+            item.classList.add("blueItem")
+        }
+
+        element.appendChild(item);
+    }
+
+
 
 }
